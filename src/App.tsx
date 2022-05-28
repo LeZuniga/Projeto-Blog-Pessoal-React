3127f3097daf33ce74bca20574163b32f496637a
LeZuniga <leh.zuniga@gmail.com>
2022-05-27 22:06:49 -0300

    add react router e atualização login

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import{ BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './componentes/estaticos/navbar/Navbar';
 import Home from './paginas/home/Home';
 import Footer from './componentes/estaticos/footer/Footer';
+import Login from './paginas/login/Login'
 import './App.css';
 
 
 function App() {
   return (
-    <>
-    <Navbar />
-    <Home />
-    <Footer />
-    </>  
+    <Router>
+      <Navbar />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes> // Antigo Switch
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+         
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
   );
 }
 
