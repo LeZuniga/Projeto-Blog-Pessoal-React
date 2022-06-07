4336883df7deb47b348f645cd2fa9ca8bef6ee2a
LeZuniga <leh.zuniga@gmail.com>
2022-06-07 18:26:13 -0300

    Adicionando temas e postagens

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,17 +1,16 @@
-import React, { useEffect, useState, ChangeEvent } from "react";
+import React, { useState, ChangeEvent, useEffect } from "react";
 import { Grid, Box, Typography, Button } from "@material-ui/core";
 import './Login.css';
 import { TextField } from "@mui/material";
-import { Link, useNavigate } from 'react-router-dom';
-import useLocalStorage from "react-use-localstorage"
-import { login } from "../../services/Service";
+import { Link, useNavigate } from 'react-router-dom'
+import useLocalStorage from 'react-use-localstorage'
 import UserLogin from "../../models/UserLogin";
+import { login } from "../../services/Service";
+
 
 function Login() {
-
     let navigate = useNavigate();
     const [token, setToken] = useLocalStorage('token');
-
     const [userLogin, setUserLogin] = useState<UserLogin>(
         {
             id: 0,
@@ -55,42 +54,34 @@ function Login() {
         <Grid container direction='row' justifyContent="center" alignItems="center">
             <Grid alignItems="center" xs={6}>
                 <Box paddingX={20}>
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <Typography variant="h3" gutterBottom color="textPrimary" component='h3' align='center' style={{ fontWeight: 'bold' }}>Entrar</Typography>
-                        <TextField id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth />
-                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                        <TextField id='usuario' value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField id='senha' value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
                         <Box marginTop={2} textAlign='center'>
-                            <Link to='/home' className="text-decorator-none">
-                                <Button type='submit' variant='contained' color='primary'>
-                                    Logar
-                                </Button>
-                            </Link>
+
+                            <Button type='submit' variant='contained' color='primary'>
+                                Logar
+                            </Button>
                         </Box>
                     </form>
                     <Box display='flex' justifyContent='center' marginTop={2}>
                         <Box marginRight={1}>
                             <Typography variant="subtitle1" gutterBottom align="center">Não tem uma conta ?</Typography>
                         </Box>
-                        <Link to='/cadastro'><Typography variant="subtitle1" gutterBottom align="center" style={{ fontWeight: "bold" }} >Cadastra-se</Typography></Link>
+                        <Link to='/cadastro'>
+                            <Typography variant="subtitle1" gutterBottom align="center" style={{ fontWeight: "bold" }} >Cadastre-se</Typography>
+                        </Link>
                     </Box>
-
                 </Box>
-
             </Grid>
             <Grid xs={6} style={{
-                backgroundImage: `url(https://imgur.com/uwjjxt0.jpg)`,
+                backgroundImage: `url(https://imgur.com/OVUjolr.jpg)`,
                 backgroundRepeat: 'no-repeat', width: "100vh", minHeight: "100vh", backgroundSize: "cover", backgroundPosition: "center"
             }}>
-
             </Grid>
-
         </Grid>
-
-
-
     );
-
 }
-
 
 export default Login;
